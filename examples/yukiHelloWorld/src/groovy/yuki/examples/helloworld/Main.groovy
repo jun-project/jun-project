21e5983f1ccf7e31fa9b1e64c7767c14d72910a9
Andrey Antukh <niwi@niwi.be>
2014-02-15 21:05:25 +0100

    Update example code with new middleware.

--- examples/yukiHelloWorld/src/groovy/yuki/examples/helloworld/Main.groovy
+++ examples/yukiHelloWorld/src/groovy/yuki/examples/helloworld/Main.groovy
@@ -1,23 +1,31 @@
 package yuki.examples.helloworld;
 
 import yuki.handler.AbstractHandler;
+import yuki.handler.Handler;
 import yuki.adapter.jetty.JettyAdapter;
+
+import yuki.middleware.QueryParamsHandler;
 
 import yuki.core.Request;
 import yuki.core.Response;
-
-import java.io.File;
 
 
 public class Main {
     public static class HelloWorldHandler extends AbstractHandler {
         public Response handle(final Request request) {
+            println "==============================";
+            println "Request.path ${request.path}"
+            println "Request.queryString ${request.queryString} ${request.queryString.class}"
+            println "Request.queryParams ${request.queryParams} ${request.queryParams.getClass()}"
             return new Response("<strong>Hóñá ääåéëþüúí</strong>", "text/html");
         }
     }
 
     public static void main(String[] args) {
-        final JettyAdapter server = new JettyAdapter(new HelloWorldHandler(), [port: 8080]);
+        final Handler helloWorldHandler = new HelloWorldHandler();
+        final Handler mainHandler = new QueryParamsHandler(helloWorldHandler);
+
+        final JettyAdapter server = new JettyAdapter(mainHandler, [port: 8080]);
         server.start();
 
         println "Now watching on http://localhost:8080/";
