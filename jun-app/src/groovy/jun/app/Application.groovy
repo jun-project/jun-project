28ad58479b3aadd378c6540bad84408f4b618ec6
Andrey Antukh <niwi@niwi.be>
2014-02-16 14:34:54 +0100

    Improved application class.

--- jun-app/src/groovy/jun/app/Application.groovy
+++ jun-app/src/groovy/jun/app/Application.groovy
@@ -1,17 +1,62 @@
 package jun.app;
 
-class Application {
-    static currentApp = new Application();
-    def name = "jun-app";
+import static jun.helpers.MiddlewareHelper.combine;
 
-    def Application() {}
+import groovy.util.ConfigSlurper;
+import jun.adapter.jetty.JettyAdapter;
 
+import jun.handler.middleware.QueryParamsHandler;
+import jun.handler.AbstractHandler;
+
+
+class Application extends AbstractHandler {
+    def initialized = false;
+    def applicationName = "jun-app";
+
+    def urlDispatchHandler;
+    def mainHandler;
+
+    // Main Constructor
     def Application(name) {
-        this.name = name;
+        this.applicationName = name;
     }
 
-    // Return current application
-    def getCurrent() {
-        return this.currentApp;
+    // Configuration
+    def setUrlDispatcher(dispatcher) {
+        this.urlDispatchHandler = dispatcher;
+    }
+
+    def makeHandler() {
+        if (!this.urlDispatchHandler) {
+            throw RuntimeException("Can not start app if url dispatcher is not specified.");
+        }
+
+        // TODO: add more default middlewares when them
+        // are implemented on jun-core module.
+        return combine(this.urlDispatchHandler,
+                       new QueryParamsHandler());
+    }
+
+    def initializeHandler() {
+        this.mainHandler = this.makeHandler();
+    }
+
+    def initializeConfig() {
+        // TODO
+    }
+
+    def initialize() {
+        println "Initialize application..."
+        this.initializeHandler();
+        this.initializeConfig();
+    }
+
+    def handle(request) {
+        if (!this.initialized) {
+            this.initialze();
+            this.initialized = true;
+        }
+
+        return this.mainHandler.handle(request);
     }
 }
